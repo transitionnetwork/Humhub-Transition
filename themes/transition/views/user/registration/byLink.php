9b4ae12f80ec781e6de47d4b70274d658ee72429
Marc Farré <contact@marc.fun>
2023-03-31 11:20:47 +0200

    update

---
+++ themes/transition/views/user/registration/byLink.php
@@ -0,0 +1,13 @@
+<?php
+require Yii::$app->getModule('user')->viewPath . '/registration/byLink.php';
+?>
+
+<style>
+    #register-form, .or-container {
+        display: none !important;
+    }
+
+    .authChoice {
+        text-align: center;
+    }
+</style>
