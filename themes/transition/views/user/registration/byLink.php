3f7759c11f16a8abcd3988538fff693cd5759959
Marc Farré <contact@marc.fun>
2023-05-20 13:33:48 +0200

    update

--- themes/transition/views/user/registration/byLink.php
+++ themes/transition/views/user/registration/byLink.php
@@ -2,11 +2,14 @@
 require Yii::$app->getModule('user')->viewPath . '/registration/byLink.php';
 
 // TODO when https://github.com/humhub/humhub/pull/6250 is in the code, do something better to redirect users to the invited space after registration (see https://helpdesk.transition-space.org/conversation/96?folder_id=23)
-$spaceId = Yii::$app->request->get('spaceId');
-if ($spaceId) {
-    $space = \humhub\modules\space\models\Space::findOne($spaceId);
-    if ($space) {
-        Yii::$app->user->setReturnUrl($space->createUrl());
+$inviteSpaceId = Yii::$app->request->get('spaceId');
+if ($inviteSpaceId) {
+    $inviteSpace = \humhub\modules\space\models\Space::findOne($inviteSpaceId);
+    if ($inviteSpace) {
+        Yii::$app->user->setReturnUrl($inviteSpace->createUrl());
+        if (Yii::$app->session->isActive) {
+            Yii::$app->session->set('invite_space_id', $inviteSpaceId);
+        }
     }
 }
 ?>
