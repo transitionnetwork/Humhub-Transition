6eae85ae06551172879799ede39679d081e08e5a
Marc Farré <contact@marc.fun>
2023-07-19 19:23:59 +0200

    Enh: Added profile embed page

--- views/embed/profile.php
+++ views/embed/profile.php
@@ -14,6 +14,7 @@
 use humhub\libs\Html;
 use humhub\modules\ui\view\components\View;
 use humhub\modules\user\widgets\Image;
+use humhub\widgets\Button;
 
 ?>
 <style>
@@ -29,11 +30,15 @@ use humhub\modules\user\widgets\Image;
 <?php if (!$user) : ?>
     <?= Yii::t('TransitionModule.base', 'No user found on Humhub') ?>
 <?php else : ?>
-    <?= Image::widget([
-        'user' => $user,
-        'width' => 35,
-        'link' => true,
-    ]) ?>
-    &nbsp;
-    <?= Html::containerLink($user) ?>
+    <?= Button::asLink(
+        Image::widget([
+            'user' => $user,
+            'width' => 35,
+            'link' => false,
+        ]) . ' &nbsp; ' .
+        Html::encode($user->getDisplayName())
+    )
+        ->link($user->createUrl(null, [], true))
+        ->loader(false)
+        ->options(['target' => '_blank']) ?>
 <?php endif; ?>
